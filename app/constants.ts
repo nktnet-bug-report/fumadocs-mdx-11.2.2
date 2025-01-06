2c4e4bfa0669fe2fa74e1a0ee0ffdf9deda037ab
Khiet Tam Nguyen <tamkhietnguyen1@gmail.com>
2025-01-06 23:37:38 +1100

    bug: code to trigger issue in 11.2.2, but not 11.2.1

---
+++ app/constants.ts
@@ -0,0 +1,1 @@
+export const SOME_VALUE = 'helloworld';
--- source.config.ts
+++ source.config.ts
@@ -1,4 +1,7 @@
+import { SOME_VALUE } from '@/app/constants';
 import { defineDocs, defineConfig } from 'fumadocs-mdx/config';
+
+console.log(SOME_VALUE);
 
 export const { docs, meta } = defineDocs({
   dir: 'content/docs',
